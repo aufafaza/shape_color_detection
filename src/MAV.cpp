23a069150302488f11c3c31f1e29274e3c204cac
aufafaza <aufa.rienaldifaza.a@gmail.com>
2026-03-14 14:23:08 +0700

    feat: connection via serial and udp

---
+++ src/MAV.cpp
@@ -0,0 +1,5 @@
+#include "MAV.hpp" 
+
+
+MAV::MAV(string port, int baud) : port_(port), baud_(baud) {}
+
--- src/MAV.hpp
+++ src/MAV.hpp
@@ -1,1 +1,17 @@
-
+#include <ardupilotmega/mavlink.h> 
+#include <string> 
+#include "mavutil/serial_port.h" 
+#include "mavutil/udp_port.h"
+using namespace std; 
+class MAV {
+	private:
+		Serial_Port serial_; 
+		UDP_Port udp_; 
+	public: 
+		MAV(string port, int baud); 
+		~MAV() = default;
+		bool connect();
+		void disconnect(); 
+		void set_servo();
+		void get_message(); 
+}; 
